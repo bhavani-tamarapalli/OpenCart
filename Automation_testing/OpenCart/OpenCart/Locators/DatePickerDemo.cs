ae945b9bad71614645813379f95d7efdb359dec0
unknown <bhavanitamarapalli123@gmail.com>
2025-10-14 18:25:41 +0530

    added calender

---
+++ Automation_testing/OpenCart/OpenCart/Locators/DatePickerDemo.cs
@@ -0,0 +1,166 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OpenCart;
+
+public class DatePickerDemo
+{
+
+
+    private IWebDriver driver;
+
+    //selecting future date
+    static void selectFutureDate(IWebDriver driver, String year, String month, String date)
+    {
+        while (true)
+        {
+            String Month = driver.FindElement(By.XPath("//span[@class='ui-datepicker-month']")).Text;//actual month
+            String Year = driver.FindElement(By.XPath("//span[@class='ui-datepicker-year']")).Text;//actual year
+            if (Month.Equals(month) && Year.Equals(year))
+            {
+                break;
+            }
+            driver.FindElement(By.XPath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).Click();//next button
+            //driver.FindElement(By.XPath("//span[@class='ui-icon ui-icon-circle-triangle-w']")).Click();//Previous button
+        }
+
+        IList<IWebElement> AllDates = driver.FindElements(By.XPath("//table[@class='ui-datepicker-calendar']//tbody//tr//td//a"));
+        foreach (IWebElement ele in AllDates)
+        {
+            if (ele.Text.Equals(date))
+            {
+                ele.Click();
+                break;
+            }
+        }
+    }
+
+    //selecting past date
+    static void selectPastDate(IWebDriver driver, String year, String month, String date)
+    {
+        while (true)
+        {
+            String Month = driver.FindElement(By.XPath("//span[@class='ui-datepicker-month']")).Text;//actual month
+            String Year = driver.FindElement(By.XPath("//span[@class='ui-datepicker-year']")).Text;//actual year
+            if (Month.Equals(month) && Year.Equals(year))
+            {
+                break;
+            }
+            //driver.FindElement(By.XPath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).Click();//next button
+            driver.FindElement(By.XPath("//span[@class='ui-icon ui-icon-circle-triangle-w']")).Click();//Previous button
+        }
+
+        IList<IWebElement> AllDates = driver.FindElements(By.XPath("//table[@class='ui-datepicker-calendar']//tbody//tr//td//a"));
+        foreach (IWebElement ele in AllDates)
+        {
+            if (ele.Text.Equals(date))
+            {
+                ele.Click();
+                break;
+            }
+        }
+    }
+
+
+
+
+    [SetUp]
+
+    public void Setup()
+    {
+        driver = new ChromeDriver();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+    }
+
+
+
+    [Test]
+    public void DatePickerDemoTest()
+    {
+        driver.Navigate().GoToUrl("https://jqueryui.com/datepicker/");
+        driver.Manage().Window.Maximize();
+
+
+        //switch to frame
+
+        driver.SwitchTo().Frame(0);
+
+        //method-1 using sendkeys
+        //driver.FindElement(By.XPath("//input[@id='datepicker']")).SendKeys("10/24/2025"); //mm//yy//yyyy
+
+        //method-2 using date picker
+        //expected data
+        //String year = "2004";
+        //String month = "Febrauary";
+        //String date = "16";
+        /*
+        String year = "2026";
+        String month = "February";
+        String date = "16";
+        */
+        driver.FindElement(By.XPath("//input[@id='datepicker']")).Click();//opens date picker
+
+        /*
+        //selecting month and year
+        while (true)
+        {
+
+            String Month = driver.FindElement(By.XPath("//span[@class='ui-datepicker-month']")).Text;//actual month
+            String Year = driver.FindElement(By.XPath("//span[@class='ui-datepicker-year']")).Text;//actual year
+
+            if (Month.Equals(month) && Year.Equals(year))
+            {
+                break;
+            }
+
+            driver.FindElement(By.XPath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).Click();//next button
+            //driver.FindElement(By.XPath("//span[@class='ui-icon ui-icon-circle-triangle-w']")).Click();//Previous button
+        }
+        */
+        /*
+        //selecting date
+
+        IList<IWebElement> AllDates = driver.FindElements(By.XPath("//table[@class='ui-datepicker-calendar']//tbody//tr//td//a"));
+        foreach (WebElement ele in AllDates)
+        {
+
+            if (ele.Text.Equals(date))
+            {
+                ele.Click();
+                break;
+            }
+        }
+
+        */
+
+        //selectMonthAndYear(driver, month, year);
+        //selectDate(driver, date);
+
+        /*
+        String year = "2026";
+        String month = "February";
+        String date = "16";
+
+        selectFutureDate(driver, year, month, date);
+        //selectFutureDate(driver, "2025", "December","25" );
+        */
+        String year = "2004";
+        String month = "February";
+        String date = "16";
+        selectPastDate(driver, year, month, date);
+
+    }
+
+
+    private bool closeBrowser = false;
+    [TearDown]
+    public void Teardown()
+    {
+        if (closeBrowser && driver != null)
+        {
+            driver.Quit();
+            driver.Dispose();
+
+        }
+    }
+}
--- Automation_testing/OpenCart/OpenCart/Locators/DynamicPaginationTable.cs
+++ Automation_testing/OpenCart/OpenCart/Locators/DynamicPaginationTable.cs
@@ -17,13 +17,60 @@ public class DynamicPaginationTable
     [Test]
     public void DynamicPaginationTableTest()
     {
-        //driver.Navigate().GoToUrl("https://practice.expandtesting.com/dynamic-pagination-table");
+        driver.Navigate().GoToUrl("https://practice.expandtesting.com/dynamic-pagination-table");
 
-        driver.Navigate().GoToUrl("https://demo.opencart.com/admin/");
+        //driver.Navigate().GoToUrl("https://testautomationpractice.blogspot.com/");
         driver.Manage().Window.Maximize();
+
+        ////showing the pages
+        //string text = driver.FindElement(By.XPath("//ul[@class='pagination']//a[contains(text(),'Previous')]")).Text;
+        //int totalPages = int.Parse(text.Substring(text.IndexOf("(") + 1, text.IndexOf("Previous") - 1));
+
+
+        ////repeating pages
+        //for (int i = 1; i <= totalPages; i++)
+        //{
+        //    if (i > 1)
+        //    {
+        //        IWebElement active_Page = driver.FindElement(By.XPath("//ul[@class='pagination']//*[text()=" + i + "]"));
+        //        active_Page.Click();
+        //    }
+
+        //    //reading data from pages
+        //}
+
+        IList<IWebElement> pages = driver.FindElements(By.XPath("//ul[@class='pagination']//a[not(contains(text(),'Previous')) and not(contains(text(),'Next'))]"));
+        int totalPages = pages.Count;
+
+        Console.WriteLine("Total Pages: " + totalPages);
+
+        for (int i = 1; i <= totalPages; i++)
+        {
+            if (i > 1)
+            {
+                // Click the page number dynamically
+                IWebElement pageLink = driver.FindElement(By.XPath($"//ul[@class='pagination']//a[text()='{i}']"));
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", pageLink);
+                Thread.Sleep(500);
+                pageLink.Click();
+            }
+
+            // Read table data on the current page
+            int noOfRows = driver.FindElements(By.XPath("//table[@class='table table-striped table-bordered dataTable no-footer']//tbody//tr")).Count;
+
+
+
+            for (int r=1; r<= noOfRows; r++)
+            {
+                string firstName = driver.FindElement(By.XPath("//table[@class='table table-striped table-bordered dataTable no-footer']//tbody//tr[" + r + "]//td[2]")).Text;
+                string lastName = driver.FindElement(By.XPath("//table[@class='table table-striped table-bordered dataTable no-footer']//tbody//tr[" + r + "]//td[3]")).Text;
+                string age = driver.FindElement(By.XPath("//table[@class='table table-striped table-bordered dataTable no-footer']//tbody//tr[" + r + "]//td[4]")).Text;
+                Console.WriteLine(firstName + "\t " + lastName + "\t " + age);
+            }
+
+
+        }
     }
-
-
     private bool closeBrowser = false;
     [TearDown]
     public void Teardown()
